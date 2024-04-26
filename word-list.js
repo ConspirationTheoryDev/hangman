57004dd0e13860066ce07b57218b9162c7d4fbcb
DESKTOP-A3VO10O\Axel Duza Knaga <trotes15@vp.pl>
2024-04-26 15:54:46 +0200

    Hangman initialize

---
+++ word-list.js
@@ -0,0 +1,195 @@
+const wordList = [
+ 
+  {
+    word: "telefon",
+    hint: "Urządzenie służące do komunikacji zdalnej.",
+  },
+  {
+    word: "kot",
+    hint: "Mały domowy drapieżnik.",
+  },
+  {
+    word: "deska",
+    hint: "Płaska powierzchnia z drewna lub innego materiału.",
+  },
+  {
+    word: "kwiatek",
+    hint: "Mała roślina ozdobna.",
+  },
+  {
+    word: "mleko",
+    hint: "Białe płynne pożywienie.",
+  },
+  {
+    word: "laptop",
+    hint: "Przenośny komputer.",
+  },
+  {
+    word: "dom",
+    hint: "Miejsce zamieszkania ludzi.",
+  },
+  {
+    word: "komputer",
+    hint: "Urządzenie elektroniczne służące do przetwarzania danych.",
+  },
+  {
+    word: "serce",
+    hint: "Organ odpowiedzialny za pompowanie krwi.",
+  },
+  {
+    word: "las",
+    hint: "Obszar zalesiony.",
+  },
+  {
+    word: "rower",
+    hint: "Środek transportu z dwoma kołami.",
+  },
+  {
+    word: "kwiat",
+    hint: "Część rośliny, która produkuje nasiona.",
+  },
+  {
+    word: "domownik",
+    hint: "Osoba mieszkająca w tym samym domu.",
+  },
+  {
+    word: "telewizor",
+    hint: "Urządzenie do odbioru sygnałów telewizyjnych.",
+  },
+  {
+    word: "kawa",
+    hint: "Napój robiony z palonych i zmielonych ziaren kawowca.",
+  },
+  {
+    word: "drzewo",
+    hint: "Organizm roślinny o drewniejącym pniu.",
+  },
+  {
+    word: "butla",
+    hint: "Pojemnik zwykle wykonany z szkła lub plastiku.",
+  },
+  {
+    word: "pies",
+    hint: "Najlepszy przyjaciel człowieka.",
+  },
+  {
+    word: "pomidor",
+    hint: "Jadalny owoc, popularny składnik w kuchni.",
+  },
+  {
+    word: "hipopotam",
+    hint: "Duże zwierzę lądowe z Afryki, znane z obfitej sylwetki.",
+  },
+  {
+    word: "filozofia",
+    hint: "Dyscyplina naukowa zajmująca się fundamentalnymi problemami ludzkiego poznania i bytu.",
+  },
+  {
+    word: "konstelacja",
+    hint: "Grupa gwiazd widoczna na niebie, tworząca określony kształt.",
+  },
+  {
+    word: "archipelag",
+    hint: "Zespół wysp rozproszonych na dużym obszarze wodnym, na przykład morzu.",
+  },
+  {
+    word: "magnetyzm",
+    hint: "Zjawisko fizyczne związane z oddziaływaniem magnesów i prądów elektrycznych.",
+  },
+  {
+    word: "paradoks",
+    hint: "Zagadnienie sprzeczne z powszechnie przyjętymi przekonaniami lub regułami logicznymi.",
+  },
+  {
+    word: "astronomia",
+    hint: "Nauka zajmująca się badaniem nieba, gwiazd, planet i innych ciał niebieskich.",
+  },
+  {
+    word: "ekwilibrystyka",
+    hint: "Sztuka wykonywania równowagi na wysokości, na przykład na linie.",
+  },
+  {
+    word: "politechnika",
+    hint: "Typ uczelni wyższej oferującej szeroki zakres nauk technicznych i inżynieryjnych.",
+  },
+  {
+    word: "biochemia",
+    hint: "Dziedzina nauki łącząca biologię i chemię, zajmująca się badaniem procesów życiowych na poziomie chemicznym.",
+  },
+  {
+    word: "hipoteza",
+    hint: "Założenie, teoria lub przypuszczenie, które można potwierdzić lub obalić poprzez badania.",
+  },
+  {
+    word: "kwantyfikacja",
+    hint: "Proces określania wielkości lub ilości czegoś.",
+  },
+  {
+    word: "katastrofizm",
+    hint: "Filozoficzna koncepcja, według której zmiany w historii Ziemi wynikają z nagłych katastroficznych wydarzeń.",
+  },
+  {
+    word: "elektroliza",
+    hint: "Proces rozkładu chemicznego substancji pod wpływem prądu elektrycznego.",
+  },
+  {
+    word: "arachnofobia",
+    hint: "Lęk lub fobia przed pająkami.",
+  },
+  {
+    word: "antykonstytucyjny",
+    hint: "Sprzeczny z postanowieniami konstytucji.",
+  },
+  {
+    word: "dezintegracja",
+    hint: "Proces rozpadu lub rozproszenia się czegoś.",
+  },
+  {
+    word: "paradoksalny",
+    hint: "Sprzeczny z intuicją lub zwykłym rozumowaniem.",
+  },
+  {
+    word: "mikrofalówka",
+    hint: "Urządzenie służące do nagrzewania potraw za pomocą fal elektromagnetycznych.",
+  },
+  {
+    word: "ekskluzywny",
+    hint: "Luksusowy i dostępny tylko dla nielicznych.",
+  },
+  {
+    word: "hipotetyczny",
+    hint: "Oparty na założeniach lub przypuszczeniach, nie na faktach.",
+  },
+  {
+    word: "konspiracja",
+    hint: "Tajne porozumienie lub spisek.",
+  },
+  {
+    word: "kapitalistyczny",
+    hint: "Związany z systemem gospodarczym opartym na prywatnej własności i zysku.",
+  },
+  {
+    word: "oligarchia",
+    hint: "Rządy niewielkiej grupy osób lub rodzin.",
+  },
+  {
+    word: "holograficzny",
+    hint: "Związany z technologią tworzenia obrazów trójwymiarowych za pomocą światła.",
+  },
+  {
+    word: "industrializacja",
+    hint: "Proces rozwoju przemysłu w danej okolicy lub kraju.",
+  },
+  {
+    word: "elektromagnetyzm",
+    hint: "Zjawisko związane z oddziaływaniem pól magnetycznych i elektrycznych.",
+  },
+  {
+    word: "kwantowy",
+    hint: "Związany z teorią fizyki opisującą zachowanie materii na poziomie subatomowym.",
+  },
+  {
+    word: "kontraktualny",
+    hint: "Opierający się na umowie lub kontrakcie.",
+  },
+];
